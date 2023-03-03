a31969b07da0b51853730c01f5470778a725efb0
dimplekhandelwal <126850386+dimplekhandelwal@users.noreply.github.com>
2023-03-03 19:25:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+// import '.APP.css';
+import ExpenseItems from './components/ExpenseItems';
+function App() {
+  return (
+    <div>
+      <h1>Lets get Started!</h1>
+      <ExpenseItems>
+      </ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
